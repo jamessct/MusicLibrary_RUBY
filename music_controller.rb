f3d80bea5d58cc280790d42ce7e68d4097fd118b
James Scott <jamessct@hotmail.co.uk>
2016-08-10 17:17:24 +0100

    initial commit

---
+++ music_controller.rb
@@ -0,0 +1,49 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry-byebug')
+require_relative('models/artist')
+
+#New
+get('/artists/new') do
+  erb(:new_artist)
+end
+
+#Create
+post('/artists') do
+  @artists = Artist.new(params)
+  @artists.save
+  erb(:create_artist)
+end
+
+#Index
+get('/artists') do
+  @artists = Artist.all
+  erb(:index_artist)
+end
+
+#Show
+get('/artists/:id/') do
+  @artists = Artist.find(params[:id])
+  erb(:show_artist)
+end
+
+
+get('/albums/new') do
+  erb(:new_album)
+end
+
+post('/albums') do
+  @albums = Album.new(params)
+  @albums.save
+  erb(:create_album)
+end
+
+get('/albums/') do
+  @albums = Album.all
+  erb(:index_album)
+end
+
+get('/albums/:id') do
+  @albums = Album.find(params[:id])
+  erb(:show_album)
+end
